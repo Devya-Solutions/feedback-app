5dc84eed996317f58165336ebb410151fbf6b59e
Ahmed Mahmoud <ahmeddahabmohamed@gmail.com>
2026-07-14 14:21:56 +0300

    chore(feedback): drop The Manifest from review platforms
    Auto-syndicates from Clutch; no separate review-routing needed.

--- lib/platforms.ts
+++ lib/platforms.ts
@@ -7,7 +7,6 @@ export const REVIEW_PLATFORMS = [
   { key: 'clutch', name: 'Clutch', url: 'https://clutch.co/profile/devya-solutions' },
   { key: 'g2', name: 'G2', url: 'https://www.g2.com/products/devya-solutions/reviews' },
   { key: 'goodfirms', name: 'GoodFirms', url: '' },
-  { key: 'the-manifest', name: 'The Manifest', url: '' },
   { key: 'trustpilot', name: 'Trustpilot', url: 'https://www.trustpilot.com/review/devya.dev' },
   { key: 'facebook', name: 'Facebook', url: 'https://www.facebook.com/devyasoftware/reviews' },
   { key: 'google', name: 'Google Business Profile', url: '' },
